918e243c6ae34197b8cdb846adc7a4c68c645781
MarcoDGreca <m.dellagreca2@studenti.unisa.it>
2024-06-01 18:52:15 +0200

    id-1 resolved

--- src/it/unisa/control/HomeServlet.java
+++ src/it/unisa/control/HomeServlet.java
@@ -1,5 +1,6 @@
 package it.unisa.control;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -19,45 +20,71 @@ import it.unisa.model.ProdottoDao;
  */
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		ProdottoDao dao = new ProdottoDao();
-		
-		ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
-		String redirectedPage = request.getParameter("page");
-		
-		try {
-			ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
-			ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
-			ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
-			ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
-			ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
-			
-			categorie.add(PS5);
-			categorie.add(XboxSeries);
-			categorie.add(Switch);
-			categorie.add(PS4);
-			categorie.add(Xone);
+    private static final long serialVersionUID = 1L;
 
-			request.getSession().setAttribute("categorie", categorie);
-			
+    // Directory consentita
+    private static final String BASE_DIRECTORY = "/WEB-INF/";
 
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
-		dispatcher.forward(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProdottoDao dao = new ProdottoDao();
+        ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
+        String redirectedPage = request.getParameter("page");
 
+        // Validazione del percorso
+        if (redirectedPage == null || !isPathAllowed(redirectedPage)) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Pagina non valida");
+            return;
+        }
+        
+        if (redirectedPage.contains("WEB-INF/web.xml")) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Pagina non valida");
+            return;
+        }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
+        String pagePath = BASE_DIRECTORY + redirectedPage;
 
+        try {
+            ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
+            ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
+            ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
+            ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
+            ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
+
+            categorie.add(PS5);
+            categorie.add(XboxSeries);
+            categorie.add(Switch);
+            categorie.add(PS4);
+            categorie.add(Xone);
+
+            request.getSession().setAttribute("categorie", categorie);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagePath);
+        dispatcher.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private boolean isPathAllowed(String path) {
+        // Verifica che il percorso non contenga sequenze che potrebbero consentire il Path Traversal
+        File file = new File(BASE_DIRECTORY, path);
+        try {
+            String canonicalPath = file.getCanonicalPath();
+            String basePath = new File(BASE_DIRECTORY).getCanonicalPath();
+
+            // Verifica che il percorso sia sotto la directory consentita e non contenga ".."
+            // Inoltre, verifica che il percorso non punti a web.xml
+            return canonicalPath.startsWith(basePath) 
+                   && !canonicalPath.contains("..") 
+                   && !canonicalPath.equals("/WEB-INF/web.xml");
+        } catch (IOException e) {
+            return false;
+        }
+    }
 }
+
